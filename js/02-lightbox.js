94395530d7e67f3b7c490dd01217eaa566576d61
Oleksii Kononenko <canon318i@gmail.com>
2022-01-31 01:35:02 +0200

    Removed var lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,11 +5,15 @@ const galleryMarkup = createGalleryPalette(galleryItems);
 const galleryRef = document.querySelector('.gallery');
 galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
 
-var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay:250});
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 
-function createGalleryPalette(galleryItems)  {
-    return galleryItems.map(({ preview, original, description }) => {return `
+function createGalleryPalette(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
         <a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" /></a>
-        `}).join('');
-};
+        `;
+    })
+    .join('');
+}
