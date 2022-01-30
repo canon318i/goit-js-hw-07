b0e7977409e0ac911e1920d06f95c3cf339a5f18
Oleksii Kononenko <canon318i@gmail.com>
2022-01-30 17:06:51 +0200

    Added first implementation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryPalette(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) { 
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) { return 'мимо' };
+    showBigImage(getBigImageUrl(event));
+};
+
+function getBigImageUrl(event) { 
+    return event.target.dataset.source
+}
+
+function showBigImage(bigImageUrl) { 
+    const instance = basicLightbox.create(`
+    <img src="${bigImageUrl}" width="800" height="600">
+    `)
+    instance.show();
+    
+    window.addEventListener("keydown", event => {
+        instance.close();
+    }, {once: true});
+}
+
+function createGalleryPalette(galleryItems) { 
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join('');
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryPalette(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay:250});
+
+function createGalleryPalette(galleryItems)  {
+    return galleryItems.map(({ preview, original, description }) => {return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" /></a>
+        `}).join('');
+};
